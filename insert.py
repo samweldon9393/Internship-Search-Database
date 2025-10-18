927aeafcd688172deb9e7ed6fce352af9c21f703
Sam <samweldon9393@gmail.com>
2025-10-17 23:49:11 -0400

    start writing python script to make insertions a bit easier

---
+++ insert.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+import sqlite3
+
+class Table(Enum):
+    applications = 1
+    companies = 2
+    contacts = 3 
+    events = 4
+
+def applications():
+    pass
+
+def companies():
+    pass
+
+def contacts():
+    pass
+
+def events():
+    pass
+
+def main():
+    print("1: applications")
+    print("2: companies")
+    print("3: contacts")
+    print("4: events")
+    table = input("Insert into which table? Enter:")
+    try:
+        table = int(table)
+    except:
+        print("Invalid input")
+        sys.exit(1)
+
+    conn = sqlite3.connect('applications.db')
+    cursor = conn.cursor()
+
+    match table:
+        case 1:
+            application(cursor)
+        case 2:
+            companies(cursor)
+        case 3:
+            contacts(cursor)
+        case 4:
+            events(cursor)
+        case _:
+            print("Invalid input")
+            sys.exit(2)
+
+if __name__='__main__':
+    main()
